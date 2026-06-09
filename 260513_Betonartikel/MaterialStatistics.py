cb0df1168e7a872a8fabe701021a2cbdbf6521eb
gat-dse <tg@dse.systems>
2026-06-09 08:34:07 +0200

    MaterialStatistics

--- 260513_Betonartikel/MaterialStatistics.py
+++ 260513_Betonartikel/MaterialStatistics.py
@@ -13,15 +13,69 @@ connection = sqlite3.connect(database_name)
 # create cursor object
 cursor = connection.cursor()
 #------------------------------------------------------------------------------------------------------------------------
-#extract values for concrete
+#extract values for concrete C20/25
 #
 inquiry = (""" 
         SELECT DENSITY, Total_GWP, Total_GWP_m3, MECH_PROP FROM products
         WHERE DENSITY IS NOT NULL
         AND "MATERIAL" LIKE '%ready_mixed_concrete%'
+        AND MECH_PROP LIKE '%C20/25%'
         AND ("Copy for strength" IS NULL OR "Copy for strength" LIKE '%a%')
-        AND MECH_PROP IS NOT NULL
-        AND MECH_PROP NOT LIKE '%GL30%'
         AND ValidEPD = 1
         AND  MIN_MAX = 1
         AND Man_Ausschluss = 1 """
+           )
+cursor.execute(inquiry)
+result = cursor.fetchall()
+
+dfC2025 = pd.DataFrame(result, columns=["DENSITY", "Total_GWP", "Total_GWP_m3", "MECH_PROP"])
+
+print(dfC2025["DENSITY"])
+print(dfC2025["Total_GWP"])
+print(dfC2025["Total_GWP_m3"])
+print(dfC2025["MECH_PROP"])
+#------------------------------------------------------------------------------------------------------------------------
+#extract values for concrete C25/30
+#
+inquiry = (""" 
+        SELECT DENSITY, Total_GWP, Total_GWP_m3, MECH_PROP FROM products
+        WHERE DENSITY IS NOT NULL
+        AND "MATERIAL" LIKE '%ready_mixed_concrete%'
+        AND MECH_PROP LIKE '%C25/30%'
+        AND ("Copy for strength" IS NULL OR "Copy for strength" LIKE '%a%')
+        AND ValidEPD = 1
+        AND  MIN_MAX = 1
+        AND Man_Ausschluss = 1 """
+           )
+cursor.execute(inquiry)
+result = cursor.fetchall()
+
+dfC2530 = pd.DataFrame(result, columns=["DENSITY", "Total_GWP", "Total_GWP_m3", "MECH_PROP"])
+
+print(dfC2530["DENSITY"])
+print(dfC2530["Total_GWP"])
+print(dfC2530["Total_GWP_m3"])
+print(dfC2530["MECH_PROP"])
+
+#------------------------------------------------------------------------------------------------------------------------
+#extract values for concrete C30/37
+#
+inquiry = (""" 
+        SELECT DENSITY, Total_GWP, Total_GWP_m3, MECH_PROP FROM products
+        WHERE DENSITY IS NOT NULL
+        AND "MATERIAL" LIKE '%ready_mixed_concrete%'
+        AND MECH_PROP LIKE '%C30/37%'
+        AND ("Copy for strength" IS NULL OR "Copy for strength" LIKE '%a%')
+        AND ValidEPD = 1
+        AND  MIN_MAX = 1
+        AND Man_Ausschluss = 1 """
+           )
+cursor.execute(inquiry)
+result = cursor.fetchall()
+
+dfC3037 = pd.DataFrame(result, columns=["DENSITY", "Total_GWP", "Total_GWP_m3", "MECH_PROP"])
+
+print(dfC3037["DENSITY"])
+print(dfC3037["Total_GWP"])
+print(dfC3037["Total_GWP_m3"])
+print(dfC3037["MECH_PROP"])
