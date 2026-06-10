d482c56d944ae9050e5b786f91b1bb6f00265eb1
Anna Hodel <hodelan@ethz.ch>
2026-06-11 00:20:50 +0200

    Ergänzung MEd

--- 260513_Betonartikel/struct_analysis.py
+++ 260513_Betonartikel/struct_analysis.py
@@ -1401,7 +1401,9 @@ class Member1D:
         self.q_rare = self.gk + self.qk  #mit qk als Leiteinwirkung [kN/m']
         self.q_freq = self.gk + self.psi[1] * self.qk # [kN/m']
         self.q_per = self.gk + self.psi[2] * self.qk # [kN/m']
-        self.q_gzt = 1.35*self.gk + 1.5*self.qk # [kN/m']
+        self.q_gzt = 1.35*self.gk + 1.5*self.qk # [kN/m'
+        self.mRd_n = self.system.alpha_m[0] * self.q_gzt * self.system.l_tot ** 2
+        self.mRd_p = self.system.alpha_m[1] * self.q_gzt * self.system.l_tot ** 2
         self.m = self.q_per / 10
         self.w_install_adm = self.system.li_max / self.requirements.lw_install
         self.w_use_adm = self.system.li_max / self.requirements.lw_use
